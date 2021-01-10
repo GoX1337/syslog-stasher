8222e592025536605550cd06e49b74ebf645785c
gox <gox.roche@gmail.com>
2021-01-10 14:46:38 +0100

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# syslog-stasher
+# Simple Syslog RFC5424 server
+
+## Example:
+```javascript
+const SyslogStasher = require('syslog-stasher');
+
+const options = {
+    port: 514,
+    maxConnections: 10
+};
+
+const syslogServer = new SyslogStasher(options);
+
+syslogServer.listen(() => {
+    console.log(`Syslog server started listening on ${options.port}...`);
+});
+```
---
+++ index.js
@@ -0,0 +1,62 @@
+const net = require('net');
+const syslogParser = require('glossy').Parse;
+
+class SyslogServer {
+
+    constructor(options) {
+        console.log("Constructor", options);
+        this.server = net.createServer();
+        this.options = options;
+        this.port = options.port || 514;
+        this.server.maxConnections = options.maxConnections || 10;
+
+        this.server.on('connection', (socket) => {
+            socket.setEncoding('utf8');
+            socket.on('data', (data) => {
+                console.log("syslog:", syslogParser.parse(data));
+            });
+
+            socket.on('drain', () => {
+                console.log('write buffer is empty now .. u can resume the writable stream');
+                socket.resume();
+            });
+
+            socket.on('error', (error) => {
+                console.log('Error : ' + error);
+            });
+
+            socket.on('timeout', () => {
+                console.log('Socket timed out !');
+                socket.end('Timed out!');
+            });
+
+            socket.on('end', (data) => {
+                console.log('Socket ended from other end!');
+                console.log('End data : ' + data);
+            });
+
+            socket.on('close', (error) => {
+                console.log('Socket closed!');
+                if (error) {
+                    console.log('Socket was closed coz of transmission error');
+                }
+            });
+        });
+
+        this.server.on('close', () => {
+            console.log('Server closed !');
+        });
+
+        this.server.on('error', (error) => {
+            console.log('Error: ' + error);
+        });
+    }
+
+    listen(cb) {
+        this.server.listen(this.port, () => {
+            cb();
+        });
+    }
+}
+
+module.exports = SyslogServer;
